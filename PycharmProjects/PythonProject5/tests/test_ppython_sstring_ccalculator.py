91d9f45031136f1d06d8f5e228bb06b850b35fff
Adem Hibbi <ademhibbi@gmail.com>
2025-10-29 21:56:54 +0100

    test tow numbere and big number returns total

--- PycharmProjects/PythonProject5/tests/test_ppython_sstring_ccalculator.py
+++ PycharmProjects/PythonProject5/tests/test_ppython_sstring_ccalculator.py
@@ -8,9 +8,13 @@ def test_single_number_returns_itself():
     assert python_add("0") == 0
     assert python_add("2") == 2
 def test_two_numbers_sum():
-    assert python_add("1,3") == 3
+    assert python_add("1,3") == 4
     assert python_add("1,2") == 3
     assert python_add("1,1") == 1
     assert python_add("0,1") == 1
     assert python_add("0,2") == 2
     assert python_add("0,3") == 3
+    assert python_add("0,10") == 10
+    assert python_add("5,15") == 20
+    assert python_add("10,30") == 40
+    assert python_add("20,40") == 60
