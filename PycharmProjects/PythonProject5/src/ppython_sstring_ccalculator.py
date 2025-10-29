4be490c673646b3cd4f8002afa86b4693e08f963
Adem Hibbi <ademhibbi@gmail.com>
2025-10-29 21:53:33 +0100

    test tow numbere returns total

--- PycharmProjects/PythonProject5/src/ppython_sstring_ccalculator.py
+++ PycharmProjects/PythonProject5/src/ppython_sstring_ccalculator.py
@@ -1,5 +1,11 @@
 def python_add(numbers: str) -> int:
     if numbers == "":
-       return 0
+        return 0
+
     if numbers.isdigit():
-       return int(numbers)
+        return int(numbers)
+
+    if "," in numbers:
+        parts = numbers.split(",")
+        if len(parts) == 2:
+            return int(parts[0]) + int(parts[1])
--- PycharmProjects/PythonProject5/tests/test_ppython_sstring_ccalculator.py
+++ PycharmProjects/PythonProject5/tests/test_ppython_sstring_ccalculator.py
@@ -7,3 +7,10 @@ def test_single_number_returns_itself():
     assert python_add("1") == 1
     assert python_add("0") == 0
     assert python_add("2") == 2
+def test_two_numbers_sum():
+    assert python_add("1,3") == 3
+    assert python_add("1,2") == 3
+    assert python_add("1,1") == 1
+    assert python_add("0,1") == 1
+    assert python_add("0,2") == 2
+    assert python_add("0,3") == 3
